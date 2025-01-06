c90193fa6f36288b21d3fdd8cd32fd12dd669d66
Eric <eric.thomas@eseo.fr>
2025-01-06 14:58:19 +0100

    V2

--- src/main/java/fr/eseo/Main.java
+++ src/main/java/fr/eseo/Main.java
@@ -2,6 +2,7 @@ package fr.eseo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Hello, World! V2");
     }
 }
