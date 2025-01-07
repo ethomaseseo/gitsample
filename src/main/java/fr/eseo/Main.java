d6f9be5e93c7f4080a4f85fe2a120b45ae5290ff
Alexander <alexanderbelhareth@gmail.com>
2025-01-07 08:10:37 +0100

    V6

--- src/main/java/fr/eseo/Main.java
+++ src/main/java/fr/eseo/Main.java
@@ -3,6 +3,6 @@ package fr.eseo;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World! V2");
+        System.out.println("Hello, World! Alexander");
     }
 }
